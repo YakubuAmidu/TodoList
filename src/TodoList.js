ae9e6d0e276e5e3ffbe4fe76fcd87939089da332
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-09 19:51:07 -0700

    Commiting changes

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  addItem(e) {}
+
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
-          <form>
+          <form onSubmit={this.addItem}>
             <input placeholder="Enter task" />
             <button type="submit">add</button>
           </form>
